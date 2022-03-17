2855056d50071da6a46b94a7d4c5d38fa2e88fe7
Batumm <nicolas.bataille@alumni.univ-avignon.fr>
2022-03-17 23:23:08 +0100

    added missing files

---
+++ bot_v_bot.py
@@ -0,0 +1,25 @@
+from DeepLearningGo.agent import naive
+from DeepLearningGo import goboard_slow
+from DeepLearningGo import gotypes
+from DeepLearningGo.utils import print_board, print_move
+import time
+
+def main():
+	board_size = 9
+	game = goboard_slow.GameState.new_game(board_size)
+	bots = {
+		gotypes.Player.black: naive.RandomBot(),
+		gotypes.Player.white: naive.RandomBot(),
+	}
+	while not game.is_over():
+		#time.sleep(0.3)
+
+		print(chr(27) + "[2J")
+		print_board(game.board)
+		bot_move = bots[game.next_player].select_move(game)
+		print_move(game.next_player, bot_move)
+		game = game.apply_move(bot_move)
+
+
+if __name__ == '__main__':
+	main()
